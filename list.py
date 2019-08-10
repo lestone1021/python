8a9ab4157e96a431267f5c52a34ea97eb643ce94
lestone1021 <53951332+lestone1021@users.noreply.github.com>
2019-08-10 22:11:52 +0800

    2.3使用list和tuple

---
+++ list.py
@@ -0,0 +1,9 @@
+L = [
+['Apple','Google','Microsoft'],
+['Jave','Python','Ruby','PHP'],
+['Adam','Bart','Lisa']
+]
+print('L=',L)
+print('L[0][0]=',L[0][0])
+print('L[1][1]=',L[1][1])
+print('L[2][2]=',L[2][2])
---
+++ listtuple.py
@@ -0,0 +1,4 @@
+list = ['1','2','3']
+tuple = ('1',['2','3'])
+print(list)
+print(tuple)
